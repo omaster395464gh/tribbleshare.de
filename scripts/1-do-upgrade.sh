73b44e0b11dc8aadbba3d57a85b876a7d79959c8
oliver1 <oliver.warz@pdv.de>
2025-08-15 15:15:50 +0200

    PhotoPrism

--- scripts/1-do-upgrade.sh
+++ scripts/1-do-upgrade.sh
@@ -5,7 +5,13 @@ pushd $PWD
 sudo apt-get update
 sudo apt-get upgrade
 cd ~/tribbleshare.de/server || exit
+
 sudo docker-compose pull
 sudo docker-compose up -d
+
+cd ~/tribbleshare.de/server/photoprism
+sudo docker-compose pull
+sudo docker-compose up -d
+
 popd
 date
