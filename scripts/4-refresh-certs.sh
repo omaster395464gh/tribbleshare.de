5e0f7c3afefd8c78d420ef37d6503771bb1b16b1
oliver1 <oliver.warz@pdv.de>
2026-04-22 22:35:35 +0200

    Automatic cert update

--- scripts/4-refresh-certs.sh
+++ scripts/4-refresh-certs.sh
@@ -4,7 +4,7 @@ pushd $PWD
 
 cd /home/admin/certs || exit
 
-echo Check certs < 30 Tage
+echo "Check certs < 30 Tage"
 openssl x509 -in tribbleshare.follow-inconnu.ts.net.crt -noout -issuer -dates
 
 if ! openssl x509 -checkend 2592000 -noout -in tribbleshare.follow-inconnu.ts.net.crt; then
